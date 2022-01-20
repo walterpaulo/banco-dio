2ba5f2e1d9e711ed03e48893fb4879636c053207
walterpaulo <walter0paulo@hotmail.com>
2022-01-20 16:06:12 -0300

    test returno.

--- src/br/com/dio/main/Main.java
+++ src/br/com/dio/main/Main.java
@@ -7,8 +7,10 @@ import br.com.dio.model.ContaPoupanca;
 public class Main {
     public static void main(String[] args) {
         Conta cc = new ContaCorrente();
+        cc.depositar(100);
+        cc.imprimrExtrato();
         Conta cp = new ContaPoupanca();
-
+        cc.transfeir(50, cp);
         cp.imprimrExtrato();
         cc.imprimrExtrato();
     }
