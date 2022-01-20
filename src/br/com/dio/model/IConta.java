ebc7aab7c81810e5c327ba2e973e0836abfc5b91
walterpaulo <walter0paulo@hotmail.com>
2022-01-20 16:59:22 -0300

    recreated.

--- src/br/com/dio/model/IConta.java
+++ src/br/com/dio/model/IConta.java
@@ -1,8 +1,8 @@
 package br.com.dio.model;
 
 public interface IConta {
-    public void sacar(double valor);
-    public void depositar(double valor);
-    public void transfeir(double valor, Conta tipoConta);
-    public void imprimrExtrato();
+        public void sacar(double valor);
+        public void depositar(double valor);
+        public void transfeir(double valor, Conta tipoConta);
+        public void imprimrExtrato();
 }
